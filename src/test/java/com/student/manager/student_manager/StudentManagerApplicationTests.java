ff1da4eb3851a3e4e314f81c9ea563b9cd8102fa
seannn1994 <email@gsfhyj.top>
2024-12-04 19:28:43 +0800

    Initial commit

---
+++ src/test/java/com/student/manager/student_manager/StudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.manager.student_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
