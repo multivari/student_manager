beed31cbcaa62f12c77b1bbe9f927b6faa06e353
seannn1994 <email@gsfhyj.top>
2024-12-04 19:48:36 +0800

    Initial commit

--- src/main/java/com/student/manager/StudentManagerApplication.java
+++ src/main/java/com/student/manager/StudentManagerApplication.java
@@ -1,4 +1,4 @@
-package com.student.manager.student_manager;
+package com.student.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/student/manager/StudentManagerApplicationTests.java
+++ src/test/java/com/student/manager/StudentManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.student.manager.student_manager;
+package com.student.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
